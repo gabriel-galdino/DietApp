e898cbd14963c6cda6a6eaddfbff56cbe4239870
Gabriel Galdino <gabrielrocknroll7@gmail.com>
2026-01-18 12:41:29 -0300

    Replace different frames by Simplebook
    Each screen was being represented by a different frame, this caused
    awkward screen changes, so the proper way to navigate between screens
    was added with Simplebook. Also add a test that verifies that when the
    button Register is clicked the app goes to the next page.

--- test/presentation_test.cc
+++ test/presentation_test.cc
@@ -1,32 +1,39 @@
 #define CATCH_CONFIG_RUNNER
 #include "presentation/presentation.h"
 
+#include <wx/app.h>
 #include <wx/filename.h>
 #include <wx/stdpaths.h>
+#include <wx/uiaction.h>
 #include <wx/wx.h>
 #include <wx/xrc/xmlres.h>
 #include <catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 #include "core/application.h"
+#include "wx/testableframe.h"
 
 class PresentationTest : public wxApp {
  public:
-  bool OnInit() override {
+  PresentationTest() {}
+
+  virtual bool OnInit() wxOVERRIDE {
     std::vector<const char*> cmd_line_args;
     for (int i = 0; i < argc; ++i) {
       cmd_line_args.push_back(wxString(argv[i]).utf8_string().c_str());
     }
     int result =
         Catch::Session().run(cmd_line_args.size(), cmd_line_args.data());
-    Exit();
     return false;
   }
 };
 
 wxIMPLEMENT_APP(PresentationTest);
 
-TEST_CASE("Presentation", "[Initialize]") {
+#if wxUSE_UIACTIONSIMULATOR
+
+TEST_CASE("Presentation", "[UI Flow]") {
+  wxXmlResource::Get()->InitAllHandlers();
   wxFileName xrc_resources(wxStandardPaths::Get().GetExecutablePath(), "");
   xrc_resources.RemoveLastDir();
   xrc_resources.RemoveLastDir();
@@ -34,18 +41,36 @@ TEST_CASE("Presentation", "[Initialize]") {
   xrc_resources.AppendDir("dietapp");
   xrc_resources.AppendDir("xrc");
   xrc_resources.SetFullName("resource.xrc");
+  wxXmlResource::Get()->Load(xrc_resources.GetFullPath());
 
   Presentation pres(nullptr);
-  REQUIRE(pres.Initialize(xrc_resources));
+  wxTestableFrame* test_frame = wxDynamicCast(
+      wxXmlResource::Get()->LoadFrame(nullptr, "MainFrame"), wxTestableFrame);
+  wxTheApp->SetTopWindow(test_frame);
+  // Use fixed position to facilitate debugging.
+  test_frame->Move(200, 200);
+  REQUIRE(pres.Initialize(xrc_resources, test_frame));
 
-  SECTION("Initialize") {
-    wxFrame* initial_frame =
-        wxXmlResource::Get()->LoadFrame(nullptr, "Initial");
-    wxFrame* register_frame =
-        wxXmlResource::Get()->LoadFrame(nullptr, "Register");
-    wxFrame* create_frame = wxXmlResource::Get()->LoadFrame(nullptr, "Create");
-    REQUIRE(initial_frame != nullptr);
-    REQUIRE(register_frame != nullptr);
-    REQUIRE(create_frame != nullptr);
+  SECTION("Initial Page") {
+    REQUIRE(pres.GetBook()->GetSelection() == 0);
+
+    EventCounter clicked(pres.GetRegisterButton(), wxEVT_BUTTON);
+
+    wxUIActionSimulator sim;
+    wxYield();
+
+    //We move in slightly to account for window decorations, we need to yield
+    //after every wxUIActionSimulator action to keep everything working in GTK
+    sim.MouseMove(pres.GetRegisterButton()->GetScreenPosition() +
+                  wxPoint(10, 10));
+    wxYield();
+
+    sim.MouseClick();
+    wxYield();
+
+    CHECK(clicked.GetCount() == 1);
+    REQUIRE(pres.GetBook()->GetSelection() == 1);
   }
 }
+
+#endif  // wxUSE_UIACTIONSIMULATOR
